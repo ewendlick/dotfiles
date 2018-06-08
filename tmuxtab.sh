39a116f1744654e2b756bc3ad78fde8861d86bd8
eli.wendlick <eli.wendlick@rakuten.com>
2018-06-08 02:27:06 +0000

    other things

--- tmuxtab.sh
+++ tmuxtab.sh
@@ -1,7 +1,22 @@
 #! /bin/sh
 # For bringing back common tabs when tmux crashes
+
+# 2
 tmux new-window -n 'intra'
+cd /home/vagrant/code/intra
+
+# 3
 tmux new-window -n 'intra-run'
+cd /home/vagrant/code/intra
+
+# 4
 tmux new-window -n 'front'
+cd /home/vagrant/code/front
+
+# 5
 tmux new-window -n 'front-run'
+cd /home/vagrant/code/front
+
+# 6
 tmux new-window -n 'choose'
+cd /home/vagrant/code/choose
